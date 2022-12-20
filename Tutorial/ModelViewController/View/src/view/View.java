14ab555f49050824444725fbda06c1233157de46
Mike Schoder <mike.schoder@students.fhnw.ch>
2022-12-20 13:11:20 +0100

    corrected to english

--- Tutorial/ModelViewController/View/src/view/View.java
+++ Tutorial/ModelViewController/View/src/view/View.java
@@ -31,13 +31,11 @@ public class View extends PuiBase<Model, Controller> {
 
     @Override
     public void setupUiToActionBindings(Controller controller) {
-        //what happens, when we interact with the hardware? -> können wir dies löschen und die unteren Zeilen verwenden?
-
         //In the view, events are detected by the GUI or PUI and thus the model is updated.
         //The MVC pattern dictates that the view does not write directly to the model
         //but calls a method in the controller that updates the model.
 
-        //At this point the function --pressButton-- from the controler class should be called
+        //At this point the function --pressButton-- from the controller class should be called
         //on the event --Button pressed--.
 
         button.onDown(controller::pressButton);
